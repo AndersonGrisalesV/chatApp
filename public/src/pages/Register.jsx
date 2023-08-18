c1f565e14094cc14cdedfe16899e4d1c2fb2f3c7
AndersonGrisalesV <grisalesvargasanderson@gmail.com>
2023-08-18 16:56:08 -0500

    August 18, 2023 login, register added front and back

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 import styled from "styled-components";
 import { ToastContainer, toast } from "react-toastify";
@@ -8,6 +8,8 @@ import axios from "axios";
 import { registerRoute } from "../utils/APIRoutes";
 
 const Register = () => {
+  const navigate = useNavigate();
+
   const [values, setValues] = useState({
     username: "",
     emails: "",
@@ -26,13 +28,19 @@ const Register = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (handleValidation()) {
-      console.log("In validation", registerRoute);
-      const { username, email, password, confirmPassword } = values;
+      const { username, email, password } = values;
       const { data } = await axios.post(registerRoute, {
         username,
         email,
         password,
       });
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+        navigate("/");
+      }
     }
   };
 
